e1193abe09ab42553931b89ef6fe82d6935fb8c0
QuentinHeinis <94958477+QuentinHeinis@users.noreply.github.com>
2022-05-15 23:24:58 +0200

    compatibilité Card / Firebase

--- src/components/CardEvent.vue
+++ src/components/CardEvent.vue
@@ -1,16 +1,16 @@
 <template>
   <div class="w-4/5 h-44 overflow-hidden rounded-4xl relative my-2">
     <div class="cardimg w-full h-full">
-          <img :src="event.sport.img" :alt="event.sport.name" class="w-full absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+          <img :src="img" :alt="name" class="w-full absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
     </div>
     <div class="relative flex flex-col -top-full w-full z-10 pl-5 pt-3">
         <div class="flex flex-col">
           <div class="relative font-bebas-neue text-4xl">
-            <p class="pl-[1px] text-white">{{ event.sport.name }}</p>
-            <span class="absolute -z-10 top-[1px] text-Orange">{{ event.sport.name }}</span>
+            <p class="pl-[1px] text-white">{{ name }}</p>
+            <span class="absolute -z-10 top-[1px] text-Orange">{{ name }}</span>
           </div>
           <div class="text-white -mt-2 pl-10">
-              <span>{{event.difficulte}}</span>
+              <span>{{difficulte}}</span>
           </div>
         </div>
         <div class="flex gap-5 mt-3">
@@ -27,15 +27,20 @@ import cardbutton from "./buttons/cardbutton.vue"
 export default {
     data() {
     return {
-      event:{
-        sport:{
-          name:"Football",
-          img:"/public/footimg.png"
-        },
-        difficulte:"Facile",
+      
+          img:"/public/footimg.png",
+      }
       },
-    }
-  },
+      props:{
+          name:{
+            Type : String,
+            default:"..."
+          },
+          difficulte:{
+            Type : String,
+            default:"facile"
+          },
+    },
   components: {
     cardbutton,
   }
