abc493bfb206effa80de67d5fb7cab627558aac6
QuentinHeinis <94958477+QuentinHeinis@users.noreply.github.com>
2022-05-17 10:02:48 +0200

    ajout loading cards

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,11 @@
 <template>
-  <div class="flex flex-col items-center">
-      <CardEvent v-for="event in ListeEvent" :key="event" :difficulte="event.difficulte" :name="event.name"></CardEvent>
+ <div class="flex flex-col items-center" v-if="ListeEvent.length < 1">
+      <div v-for="i in 5" :key="i" class="w-4/5 h-44 overflow-hidden rounded-4xl relative my-2 is-loading"></div>
   </div>
+  <div class="flex flex-col items-center" v-if="ListeEvent.length > 0">
+      <CardEvent v-for="event in ListeEvent" :key="event" :difficulte="event.difficulte" :name="event.name" @click.prevent="deleteEvent(event)"></CardEvent>
+  </div>
+ 
 
 </template>
 
@@ -9,6 +13,11 @@
 import Header from "../components/Header.vue"
 import CardEvent from "../components/CardEvent.vue"
 import JoinBtn from "../components/buttons/btn.vue"
+import {
+    getAuth, 
+    signInWithEmailAndPassword, 
+    signOut, 
+    onAuthStateChanged } from 'https://www.gstatic.com/firebasejs/9.8.1/firebase-auth.js'
 import {
     getFirestore,
     collection,
@@ -23,6 +32,10 @@ export default {
     data(){
       return{
         ListeEvent:[],
+        user:{
+          email:null,
+          password:null,
+        }
 
       }
     },
@@ -30,13 +43,33 @@ export default {
       this.getEventSynchro();
     },
     methods:{
-      async getEventSynchro(){
+        async getEventSynchro(){
             const firestore = getFirestore();
             const dbEvent = collection(firestore, "Event");
             const query = await onSnapshot(dbEvent, (snapshot) =>{
                 this.ListeEvent = snapshot.docs.map(doc => ({id:doc.id, ...doc.data()}));
             })
         },
+        async deleteEvent(event){
+            const firestore = getFirestore();
+            const docRef = doc(firestore, "Event", event.id);
+            await deleteDoc(docRef);
+        },
     },
 }
 </script>
+
+<style>
+@keyframes shine {
+  to {
+    background-position-x: -200%;
+  }
+}
+.is-loading {
+    background: #eee;
+    background: linear-gradient(110deg, #ececec 8%, #f5f5f5 18%, #ececec 33%);
+    border-radius: 5px;
+    background-size: 200% 100%;
+    animation: 1.5s shine linear infinite;
+  }
+</style>
