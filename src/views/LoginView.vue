d421877945fe0d4a268e776e496939e504f080db
QuentinHeinis <94958477+QuentinHeinis@users.noreply.github.com>
2022-06-07 14:13:58 +0200

    login avec mitt

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,45 +1,80 @@
 <template>
-    <form @submit.prevent="onCnx()" class="w-4/5 m-auto mt-4">
-        <div class="mb-3 flex rounded-sm overflow-hidden">            
-            <div class="h-full bg-white text-black p-1 w-1/4 min-w-fit">
-                <span>Email :</span>
+<form @submit.prevent="onCnx()" class="w-4/5 m-auto mt-4" v-if="!SignIn">
+            <div class="mb-3 flex rounded-sm overflow-hidden">            
+                <div class="h-full bg-slate-200 text-black p-1 w-1/4 min-w-fit">
+                    <span>Email :</span>
+                </div>
+                <input type="text" class="w-full" v-model="user.email" required />
             </div>
-            <input type="text" class="w-full" v-model="user.email" required />
-        </div>
-        <div class="flex mb-3 rounded-sm overflow-hidden">
-            <div class="bg-white text-black p-1 w-1/4 min-w-fit">
-                <span>Mot de passe :</span>
+            <div class="flex mb-3 rounded-sm overflow-hidden">
+                <div class="bg-slate-200 text-black p-1 w-1/4 min-w-fit">
+                    <span>Mot de passe :</span>
+                </div>
+                <input type="password" class="w-full" v-model="user.password" required />
             </div>
-            <input type="password" class="w-full" v-model="user.password" required />
-        </div>
-        <div class="flex mb-3 rounded-sm overflow-hidden" v-if="SignIn">
-            <div class="bg-white text-black p-1 w-1/4 min-w-fit">
-                <span>Répéter le Mot de passe :</span>
+            <div class="bg-yellow-100 text-center mb-3 py-2" v-if="message!=null" >
+            {{ message }}
             </div>
-            <input type="password" class="w-full" v-model="user.password2" required />
-        </div>
-        <div class="bg-yellow-100 text-center mb-3 py-2" v-if="message!=null" >
-        {{ message }}
-        </div>
-        <div class="flex items-center gap-1">
-            <input type="checkbox" :checked="keeper" @click="keeper=!keeper">
-            <span class="text-white text-xs">rester connecter</span>
-        </div>
-        <div class="text-white font-oswald flex justify-evenly">
-        <button class="text-center  font-light text-xs" type="submit" @click="SignIn=!SignIn" v-if="!SignIn">
-            Créer un compte
-        </button>
-        <button class="text-center  font-light text-xs" type="submit" @click="SignIn=!SignIn" v-if="SignIn">
-            Se Connecter
-        </button>
-        <button  v-if="!SignIn" @click="onCnx()"><btn>Connexion</btn></button>
-        <button  v-if="SignIn" @click="onCreate()"><btn>Créer</btn></button>
-        </div>
-        <div class="w-full h-10 bg-blue-500 text-white" v-if="logged"></div>
-    </form>
+            <div class="text-white font-oswald flex justify-evenly">
+            <button class="text-center  font-light text-xs"  @click="SignIn=!SignIn">
+                Créer un compte
+            </button>
+            <button type="submit"><btn>Connexion</btn></button>
+            </div>
+        </form>
+        <form @submit.prevent="onCreate()" class="w-4/5 m-auto mt-4" v-if="SignIn">
+            <div class="mb-3 flex rounded-sm overflow-hidden">            
+                <div class="h-full bg-slate-200 text-black p-1 w-1/4 min-w-fit">
+                    <span>Login :</span>
+                </div>
+                <input type="text" class="w-full" v-model="user.login" required />
+            </div>
+            <div class="mb-3 flex rounded-sm overflow-hidden">            
+                <div class="h-full bg-slate-200 text-black p-1 w-1/4 min-w-fit">
+                    <span>Email :</span>
+                </div>
+                <input type="text" class="w-full" v-model="user.email" required />
+            </div>
+            <div class="flex mb-3 rounded-sm overflow-hidden">
+                <div class="bg-slate-200 text-black p-1 w-1/4 min-w-fit">
+                    <span>Mot de passe :</span>
+                </div>
+                <input type="password" class="w-full" v-model="user.password" required />
+            </div>
+            <div class="flex mb-3 rounded-sm overflow-hidden" >
+                <div class="bg-slate-200 text-black p-1 w-1/4 min-w-fit">
+                    <span>Répéter le Mot de passe :</span>
+                </div>
+                <input type="password" class="w-full" v-model="password2" required />
+            </div>
+            <div class="bg-yellow-100 text-center mb-3 py-2" v-if="message!=null" >
+            {{ message }}
+            </div>
+            <div class="text-white font-oswald flex justify-evenly">
+            <button class="text-center  font-light text-xs" type="button" @click="SignIn=!SignIn">
+                Se Connecter
+            </button>
+            <button type="submit"><btn>Créer</btn></button>
+            </div>
+        </form>
 </template>
 
 <script>
+import {emitter} from '../main.js'
+import { 
+    getFirestore,   // Obtenir le Firestore
+    collection,     // Utiliser une collection de documents
+    doc,            // Obtenir un document par son id
+    getDocs,        // Obtenir la liste des documents d'une collection
+    getDoc,
+    
+    addDoc,         // Ajouter un document à une collection
+    updateDoc,      // Mettre à jour un document dans une collection
+    deleteDoc,      // Supprimer un document d'une collection
+    onSnapshot,     // Demander une liste de documents d'une collection, en les synchronisant
+    query,          // Permet d'effectuer des requêtes sur Firestore
+    orderBy         // Permet de demander le tri d'une requête query
+    } from 'https://www.gstatic.com/firebasejs/9.8.1/firebase-firestore.js'
 import {
     getAuth, 
     signInWithEmailAndPassword, 
@@ -56,75 +91,72 @@ export default {
     components:{btn},
     data(){
         return{
-            keeper:false,
             SignIn:false,
             user:{
-                email:null,
                 password:null,
-                password2:null,
+                email:null,
+                login:null,
             },
+            password2:null,
             message:null,
-            logged:false,
         }
     },
     methods:{
+        // onCnx(){
+        //     signInWithEmailAndPassword(getAuth(), this.user.email, this.user.password)
+        //     .then((response)=>{
+        //         console.log('user connecté', response.user);
+        //         this.user = response.user;
+        //         this.message = "User connecté : " +this.user.email;
+        //     })
+        //     .catch((error)=>{
+        //         console.log('erreur de connexion', error);
+        //         this.message = "Erreur d'authentification";
+        //     })
+        // },
         onCnx(){
             signInWithEmailAndPassword(getAuth(), this.user.email, this.user.password)
-            .then((response)=>{
-                console.log('user connecté', response.user);
+            .then((response) =>{
                 this.user = response.user;
-                this.message = "User connecté : " +this.user.email;
-                this.logged = true;
+                console.log("user", this.user);
+                emitter.emit('connectUser', {user: this.user});
+                this.message = "user connecté : " + this.user.email;
             })
             .catch((error)=>{
-                console.log('erreur de connexion', error);
-                this.message = "Erreur d'authentification";
+                console.log('erreur connexion', error);
+                this.message = "erreur d'authentification";
             })
         },
-        
         onCreate(){
-            if(this.user.password==this.user.password2){
+            if(this.user.password===this.password2){
                 createUserWithEmailAndPassword(getAuth(), this.user.email, this.user.password)
                     .then((response) => {
                         // Signed in 
                         const user = response.user;
+                        this.writeUserData(user);
                         // ...
                     })
-                    .catch((errorr) => {
-                        console.log('erreur création', errorr);
+                    .catch((error) => {
+                        console.log('erreur création', error);
                         this.message = "erreur de création";
                         // ..
-               })
-               signInWithEmailAndPassword(getAuth(), this.user.email, this.user.password)
-            .then((response)=>{
-                console.log('user connecté', response.user);
-                this.user = response.user;
-                this.message = "User connecté : " +this.user.email;
-                
-            })
-            .catch((error)=>{
-                console.log('erreur de connexion', error);
-                this.message = "Erreur d'authentification";
-            })
+                    })  
             }else{
                 this.message = "password pas cohérent"
                 console.log('mdp mauvais')
             }
         },
-
-        keepLogin(){
-          if(this.keeper = true){
-              setPersistence(auth, inMemoryPersistence)
-              .then(() =>{
-                  const provider = new GoogleAuthProvider();
-                  return signInWithRedirect(auth,  provider);
-              })
-              .catch((error) => {
-                  console.log('erreurCode : '+ error.code);
-                  console.log('error message : ' + error.message);
-                  this.message = 'error message : ' + error.message;
-              })
-          }  
+        async writeUserData(user){
+                const firestore = getFirestore();
+                const dbUser = collection(firestore, "user");
+                const docRef = await addDoc(dbUser,{
+                    login: this.user.login,
+                    email:this.user.email,
+                    uid: user.uid,
+                    admin:false
+                })
+                console.log('document créé avec le id : ', docRef.id);
+                emitter.emit('connectUser', {user: this.user}); 
         }
     },
 }
