121acde5ae71323028669749aa298085828f6b09
QuentinHeinis <94958477+QuentinHeinis@users.noreply.github.com>
2022-06-07 14:13:15 +0200

    suppréssion freepremiumvue

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,11 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
-import ProfilView from '../views/ProfilView.vue'
+import ProfilView from '../views/profil/ProfilView.vue'
+import EditProfilView from '../views/profil/editProfilView.vue'
 import CreateEventView from '../views/CreateEventView.vue'
 import MyEventView from '../views/MyEventView.vue'
 import FavView from '../views/FavView.vue'
 import CarteView from '../views/CarteView.vue'
-import FreePremiumView from '../views/FreePremiumView.vue'
 import ContactView from '../views/ContactView.vue'
 import EventView from '../views/EventView.vue'
 
@@ -13,12 +13,12 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     { path: '/', name: 'HomeView', component: HomeView },
-    { path: '/Profil', name: 'ProfilView', component: ProfilView },
+    { path: '/Profil/:id', name: 'ProfilView', component: ProfilView },
+    { path: '/EditProfil/:id', name: 'EditProfilView', component: EditProfilView },
     { path: '/Create', name: 'CreateEventView', component: CreateEventView },
     { path: '/MyEvent', name: 'MyEventView', component: MyEventView },
     { path: '/Favoris', name: 'FavView', component: FavView },
     { path: '/Carte', name: 'CarteView', component: CarteView },
-    { path: '/FreePremium', name: 'FreePremiumView', component: FreePremiumView },
     { path: '/Contact', name: 'ContactView', component: ContactView },
     { path: '/Event/:id', name: 'EventView', component: EventView },
 
